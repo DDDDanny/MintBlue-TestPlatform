ea6fd06f72a0ed3f9cd04a568d8e98b3e731c1a4
DDDDanny <707956456@qq.com>
2020-11-12 11:32:21 +0800

    perf：update code style

--- src/menu/index.js
+++ src/menu/index.js
@@ -5,7 +5,8 @@ import { uniqueId } from 'lodash'
  * @description https://github.com/d2-projects/d2-admin/issues/209
  * @param {Array} menu 原始的菜单数据
  */
-function supplementPath (menu) {
+
+function supplementPath(menu) {
   return menu.map(e => ({
     ...e,
     path: e.path || uniqueId('d2-menu-empty-'),
@@ -17,7 +18,7 @@ function supplementPath (menu) {
 
 export const menuHeader = supplementPath([
   { path: '/index', title: '首页', icon: 'home' },
-  { path: '/projectManage', title: '项目管理', icon: 'flask' },
+  { path: '/projectManage', title: '项目管理', icon: 'flask' }
 ])
 
 export const menuAside = supplementPath([
@@ -29,14 +30,14 @@ export const menuAside = supplementPath([
       { path: '/apiTestCase', title: '测试用例管理', icon: 'bug' },
       { path: '/apiTestSuite', title: '测试集管理', icon: 'sitemap' },
       { path: '/apiTestTask', title: '测试任务管理', icon: 'tasks' },
-      { path: '/apiParamSetting', title: '测试参数设置', icon: 'cog' },
+      { path: '/apiParamSetting', title: '测试参数设置', icon: 'cog' }
     ]
   },
   {
     title: 'UI自动化测试',
     icon: 'object-group',
     children: [
-      { path: '/uiTestController', title: 'UI自动化测试控制器', icon: 'hand-peace-o' },
+      { path: '/uiTestController', title: 'UI自动化测试控制器', icon: 'hand-peace-o' }
     ]
   },
   {
@@ -44,7 +45,7 @@ export const menuAside = supplementPath([
     icon: 'area-chart',
     children: [
       { path: '/apiTestReport', title: '接口自动化测试报告', icon: 'bar-chart' },
-      { path: '/uiTestReport', title: 'UI自动化测试报告', icon: 'pie-chart' },
+      { path: '/uiTestReport', title: 'UI自动化测试报告', icon: 'pie-chart' }
     ]
   }
 ])
