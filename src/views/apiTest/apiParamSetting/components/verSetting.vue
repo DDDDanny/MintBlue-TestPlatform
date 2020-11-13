2a9ae3d7b2a2137ffad59cd9bcddb2de4e4a850b
DDDDanny <707956456@qq.com>
2020-11-13 22:21:39 +0800

    feat: create add Version logic

--- src/views/apiTest/apiParamSetting/components/verSetting.vue
+++ src/views/apiTest/apiParamSetting/components/verSetting.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row justify="end" type="flex" class="button-bar">
       <el-col :span="3" :offset="3">
-        <el-button type="primary">新增版本</el-button>
+        <el-button type="primary" @click="addVersion">新增版本</el-button>
       </el-col>
     </el-row>
     <el-row justify="center">
@@ -21,6 +21,20 @@
         </el-table>
       </el-col>
     </el-row>
+    <el-dialog title="新增版本号" :visible.sync="versionDialogDisplay" width="50%" @close="handleCancelAddVer">
+      <el-form :model="versionInfoForm" :rules="versionInfoRules" ref="versionRuleForm" label-width="80px">
+        <el-form-item label="版本号" prop="version">
+          <el-input v-model="versionInfoForm.version" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="版本信息" prop="verDesc">
+          <el-input v-model="versionInfoForm.verDesc" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="handleCancelAddVer">取 消</el-button>
+        <el-button type="primary" @click="handleSubmitVer">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -34,7 +48,23 @@ export default {
         { version: 'V2.11.0', verInfo: '版本信息2', createTime: '2020.11.13', creator: 'DDDDanny' }
       ],
       // 表格表头数据
-      tableHeaderColor: { background: '#FAFAFA' }
+      tableHeaderColor: { background: '#FAFAFA' },
+      // 控制弹框是否显示
+      versionDialogDisplay: false,
+      // 版本信息Form表单
+      versionInfoForm: {
+        version: '',
+        verDesc: ''
+      },
+      // 版本信息校验规则
+      versionInfoRules: {
+        version: [
+          { required: true, message: '请输入版本号', trigger: 'blur' }
+        ],
+        verDesc: [
+          { required: true, message: '请输入版本信息', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -52,6 +82,31 @@ export default {
       }).catch(() => {
         this.$message.info('您已取消删除～')
       })
+    },
+    // 新增版本信息
+    addVersion () {
+      this.versionDialogDisplay = true
+    },
+    // 处理提交版本信息
+    handleSubmitVer () {
+      this.$refs.versionRuleForm.validate((valid) => {
+        if (!valid) {
+          this.$message.error('请填写必填项！')
+          return
+        }
+        this.$message.success('新增版本号成功！')
+        // 隐藏弹框
+        this.versionDialogDisplay = false
+        // 重置数据
+        this.$refs.versionRuleForm.resetFields()
+      })
+    },
+    // 处理取消新增
+    handleCancelAddVer () {
+      // 隐藏弹框
+      this.versionDialogDisplay = false
+      // 重置数据
+      this.$refs.versionRuleForm.resetFields()
     }
   },
 }
