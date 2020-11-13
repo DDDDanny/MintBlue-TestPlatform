1d692aa39cc74dc0dea4e902066201a3c47aacd9
DDDDanny <707956456@qq.com>
2020-11-13 23:25:46 +0800

    feat: create add Env logic

--- src/views/apiTest/apiParamSetting/components/envSetting.vue
+++ src/views/apiTest/apiParamSetting/components/envSetting.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row justify="end" type="flex" class="button-bar">
       <el-col :span="3" :offset="3">
-        <el-button type="primary">新增环境</el-button>
+        <el-button type="primary" @click="addEnv">新增环境</el-button>
       </el-col>
     </el-row>
     <el-row justify="center">
@@ -21,12 +21,26 @@
         </el-table>
       </el-col>
     </el-row>
+    <el-dialog title="新增环境参数" :visible.sync="envDialogDisplay" width="50%" @close="handleCancelAddEnv">
+      <el-form :model="envInfoForm" :rules="envInfoRules" ref="envRuleForm" label-width="80px">
+        <el-form-item label="环境名称" prop="envName">
+          <el-input v-model="envInfoForm.envName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="基础地址" prop="baseURL">
+          <el-input v-model="envInfoForm.baseURL" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="handleCancelAddEnv">取 消</el-button>
+        <el-button type="primary" @click="handleSubmitEnv">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'verSetting',
+  name: 'envSetting',
   data() {
     return {
       envInfo: [
@@ -34,7 +48,23 @@ export default {
         { envName: '测试环境', baseURL: 'https://dev.xiaojing0.com/workbench_api', createTime: '2020.11.13', creator: 'DDDDanny' }
       ],
       // 表格表头数据
-      tableHeaderColor: { background: '#FAFAFA' }
+      tableHeaderColor: { background: '#FAFAFA' },
+      // 控制弹框是否显示
+      envDialogDisplay: false,
+      // 环境信息Form表单
+      envInfoForm: {
+        envName: '',
+        baseURL: ''
+      },
+      // 环境信息校验规则
+      envInfoRules: {
+        envName: [
+          { required: true, message: '请输入环境名称', trigger: 'blur' }
+        ],
+        baseURL: [
+          { required: true, message: '请输入基础地址', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -52,6 +82,29 @@ export default {
       }).catch(() => {
         this.$message.info('您已取消删除～')
       })
+    },
+    // 新增环境信息
+    addEnv () {
+      this.envDialogDisplay = true
+    },
+    // 处理提交环境信息
+    handleSubmitEnv () {
+      this.$refs.envRuleForm.validate((valid) => {
+        if (!valid) {
+          this.$message.error('请填写必填项！')
+          return
+        }
+        this.$message.success('新增环境信息成功')
+        this.envDialogDisplay = false
+        this.$refs.envRuleForm.resetFields()
+      })
+    },
+    // 处理取消新增
+    handleCancelAddEnv () {
+      // 隐藏弹框
+      this.envDialogDisplay = false
+      // 重置数据
+      this.$refs.envRuleForm.resetFields()
     }
   },
 }
