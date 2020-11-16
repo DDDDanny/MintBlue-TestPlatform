7c9ffab103494ea74426eab354f91244f8c8e02f
DDDDanny <707956456@qq.com>
2020-11-16 23:16:49 +0800

    feat: add temp page btn bar

--- src/views/apiTest/apiTestCase/tempPage.vue
+++ src/views/apiTest/apiTestCase/tempPage.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
-      <el-drawer :visible="drawerVisible" size="800px" :append-to-body="true"  @close="handleClose">
+      <el-drawer :visible="drawerVisible" size="800px" :append-to-body="true" @close="handleClose">
         <template slot="title">
-          <h3>新增测试用例</h3>
+          <span class="temp-title">新增测试用例</span>
         </template>
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="活动名称">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="btn-group">
+          <div class="temp-page-btn-bar">
+            <el-button @click="handleClose" >取消</el-button>
+            <el-button type="primary">提交</el-button>
+          </div>
+        </div>
       </el-drawer>
     </div>
 </template>
@@ -21,6 +32,13 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      form: {
+        name: ''
+      }
+    }
+  },
   methods: {
     handleClose () {
       this.$emit('handleClose')
@@ -32,5 +50,25 @@ export default {
   // 解决element ui drawer组件出现篮框的Bug
   ::v-deep :focus {
     outline: 0;
+  }
+  .temp-page-btn-bar {
+    position: relative;
+    left: 450px;
+    .el-button {
+      width: 150px;
+      margin-right: 20px;
+    }
+  }
+  .btn-group {
+    position: fixed;
+    bottom: 0px;
+    padding: 20px 0px;
+    display: inline-block;
+    width: 100%;
+    box-shadow: 0px -1px 4px 0px rgba(195, 195, 195, 0.5);
+  }
+  .temp-title {
+    color: #000000; 
+    font-size:20px  
   }
 </style>
