f0ea3b37e591b6931ba60e6ba0e2297e2b9760ec
DDDDanny <707956456@qq.com>
2020-11-17 18:32:03 +0800

    feat: add assert group components

--- src/views/apiTest/apiTestCase/addCaseForm.vue
+++ src/views/apiTest/apiTestCase/addCaseForm.vue
@@ -4,37 +4,48 @@
       <el-row>
         <el-col :span="22" :offset="1" >
           <el-form-item label="用例名称">
-            <el-input v-model="addCaseForm.caseName"></el-input>
+            <el-input v-model="addCaseForm.caseName" placeholder="请输入用例名称"></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="10" :offset="1">
           <el-form-item label="用例等级">
-            <el-input v-model="addCaseForm.caseLevel"></el-input>
+            <el-select v-model="addCaseForm.caseLevel" placeholder="请选择">
+              <el-option v-for="item in caseLevel" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="10" :offset="2">
           <el-form-item label="请求方式">
-            <el-input v-model="addCaseForm.requestMethod"></el-input>
+            <el-select v-model="addCaseForm.requestMethod" placeholder="请选择">
+              <el-option v-for="item in requestMethod" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="22" :offset="1">
           <el-form-item label="请求URL">
-            <el-input v-model="addCaseForm.requestUrl"></el-input>
+            <el-input v-model="addCaseForm.requestUrl" placeholder="请输入请求URL"></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="22" :offset="1">
           <el-form-item label="请求头">
-            <el-input v-model="addCaseForm.requestHeader"></el-input>
+            <el-input v-model="addCaseForm.requestHeader" placeholder="请输入请求头"></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="22" :offset="1">
           <el-form-item label="请求内容">
-            <el-input v-model="addCaseForm.requestBody"></el-input>
+            <el-input type="textarea" :rows="5" v-model="addCaseForm.requestBody" placeholder="请输入请求内容"></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="22" :offset="1">
-          <el-form-item label="断言信息">
-            <el-input v-model="addCaseForm.assert"></el-input>
+          <el-form-item label="断言条件">
+            <el-row>
+              <el-col :span="23">
+                <assert-group v-model="addCaseForm.assertInfo"/>
+              </el-col>
+              <el-col :span="1">
+                <el-button type="primary" size="mini" class="el-icon-plus" circle></el-button>
+              </el-col>
+            </el-row>
           </el-form-item>
         </el-col>
         <el-col :span="22" :offset="1">
@@ -44,7 +55,7 @@
         </el-col>
         <el-col :span="22" :offset="1">
           <el-form-item label="备注">
-            <el-input v-model="addCaseForm.remark"></el-input>
+            <el-input v-model="addCaseForm.remark" placeholder="请输入备注"></el-input>
           </el-form-item>
         </el-col>
       </el-row>
@@ -57,10 +68,11 @@
 </template>
 <script>
 import tempPage from '@/components/tempPage'
+import assertGroup from '@/views/apiTest/apiTestCase/assertGroup'
 
 export default {
   name: 'addCaseForm',
-  components: { tempPage },
+  components: { tempPage, assertGroup },
   props: {
     drawerVisible: {
       type: Boolean,
@@ -70,6 +82,15 @@ export default {
   data () {
     return {
       addCaseForm: {},
+      // 测试用例等级下拉选项
+      caseLevel: [
+        { value: 1, label: '低'}, { value: 2, label: '中'}, { value: 3, label: '高'}
+      ],
+      // 请求方式下拉选项
+      requestMethod: [
+        { value: 'post', label: 'POST'}, { value: 'get', label: 'GET'}
+      ],
+      xxx: {}
     }
   },
   methods: {
---
+++ src/views/apiTest/apiTestCase/assertGroup.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-form :model="assertInfo">
+    <el-row>
+      <el-col :span="7">
+        <el-form-item label="">
+          <el-input v-model="assertInfo.assertKey" placeholder="输入断言Key"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7" :offset="1">
+        <el-form-item label="">
+          <el-select v-model="assertInfo.assertCondition" placeholder="请选择条件">
+            <el-option v-for="item in assertConditions" :key="item.value" :label="item.label" :value="item.value" />
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="7" :offset="1">
+        <el-form-item label="">
+          <el-input v-model="assertInfo.assertValue" placeholder="输入断言Val"></el-input>
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+<script>
+export default {
+  name: 'assertGroup',
+  data() {
+    return {
+      assertInfo: {},
+      assertConditions: [
+        { value: '==', label: '等于' },
+        { value: '!=', label: '不等于' },
+        { value: 'In', label: '包含' },
+        { value: 'notIn', label: '不包含' },
+      ]
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
