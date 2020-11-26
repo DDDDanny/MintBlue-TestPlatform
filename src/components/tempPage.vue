51bb2d11087b1a8f568da320afbc571ad9d0496f
DDDDanny <707956456@qq.com>
2020-11-26 17:24:58 +0800

    perf: revise temp page width

--- src/components/tempPage.vue
+++ src/components/tempPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-drawer :visible="drawerVisible" size="600px" class="drawer" :append-to-body="true" @close="handleClose">
+  <el-drawer :visible="drawerVisible" size="50%" class="drawer" :append-to-body="true" @close="handleClose">
     <template slot="title">
       <span class="temp-title">新增测试用例</span>
     </template>
