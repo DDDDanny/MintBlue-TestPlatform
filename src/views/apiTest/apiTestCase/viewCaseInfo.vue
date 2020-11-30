5b3f72238e9f217c0ec159c91c8f9b08bd7a1595
DDDDanny <707956456@qq.com>
2020-12-01 00:26:43 +0800

    feat: add viewCaseInfo base UI

--- src/views/apiTest/apiTestCase/viewCaseInfo.vue
+++ src/views/apiTest/apiTestCase/viewCaseInfo.vue
@@ -1,6 +1,11 @@
 <template>
   <temp-page :drawer-visible="drawerVisible" :temp-page-title="tempPageTitle" @handleClose="handleClose">
-    <div>这里写数据展示的信息</div>
+    <el-form label-width="100px" >
+      <el-form-item v-for="(item, index) in caseInfoDetail" :key="index" :label="item.title" class="detail-form-item">
+        <el-input v-if="item.title !== '请求内容'" v-model="item.value" disabled></el-input>
+        <el-input v-else type="textarea" v-model="item.value" disabled></el-input>
+      </el-form-item>
+    </el-form>
   </temp-page>
 </template>
 <script>
@@ -17,7 +22,18 @@ export default {
   },
   data () {
     return {
-      tempPageTitle: '用例详情'
+      tempPageTitle: '用例详情',
+      caseInfoDetail: [
+        { title: '用例名称', value: 'Api测试用例' },
+        { title: '用例等级', value: '高' },
+        { title: '请求方式', value: 'POST' },
+        { title: '请求URL', value: 'WWW.baidu.com' },
+        { title: '请求头', value: '{Header: xml/json}' },
+        { title: '请求内容', value: '132' },
+        { title: '断言条件', value: 'status.msg == 登录成功' },
+        { title: '获取参数', value: 'Msg = status.msg' },
+        { title: '备注信息', value: 'Api测试用例详情' }
+      ]
     }
   },
   methods: {
@@ -29,5 +45,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+  .detail-form-item {
+    margin-right: 20px
+  }
 </style>
