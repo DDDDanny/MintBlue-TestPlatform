19067730a826981f4c7625680c0e6330fa18d388
DDDDanny <707956456@qq.com>
2021-02-02 15:41:39 +0800

    feat: add creat task api

--- src/api/modules/task.api.js
+++ src/api/modules/task.api.js
@@ -1,4 +1,6 @@
 export default ({ request }) => ({
   // 获取任务信息列表
-  listTask (params = {}) { return request({ url: '/task/list', method: 'get', params }) }
+  listTask (params = {}) { return request({ url: '/task/list', method: 'get', params }) },
+  // 新增任务信息
+  addTask (data = {}) { return request({ url: '/task/add', method: 'post', data }) },
 })
